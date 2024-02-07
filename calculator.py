048e8e1adf70efee83dde74099b9500ed238991a
editor-5 <kdcd2022@naver.com>
2024-02-07 19:46:51 +0900

    Add square function

--- calculator.py
+++ calculator.py
@@ -8,7 +8,13 @@ def subtract(a, b):
 def multiply(a, b):
     return a*b
 
-def divide_premium(a, b):
+def divide_free(a, b):
     return a/b    
 
+def square(a):
+    return a*a   
+
+
     # ex001
+    # ex002
+    # ex003
