91c0f78d977c1ade906bc8d94bc6bcca967c0b69
editor-5 <kdcd2022@naver.com>
2024-02-18 10:20:32 +0900

    202402181020

---
+++ office/근무표완성중(202403월)주간만.py
@@ -0,0 +1,51 @@
+
+a = {'주간1': '황인기', '주간2': '유현주', '휴무': '무무무'}
+b = {'주간1': '황인기', '주간2': '무무무', '휴무': '유현주'}
+c = {'주간1': '무무무', '주간2': '유현주', '휴무': '황인기'}
+
+data = [a, b, c]
+
+for _ in range(3):
+
+  print(data[data.index(c)])   #1
+  
+for _ in range(1):
+  
+  print(data[data.index(a)])   #4
+  print(data[data.index(b)])   #5
+    
+for _ in range(4):
+
+  print(data[data.index(a)])   #6
+  
+for _ in range(1):
+
+  print(data[data.index(c)])  #10
+  print(data[data.index(a)])  #11
+  print(data[data.index(b)])  #12
+   
+for _ in range(3):
+
+  print(data[data.index(a)])  #13
+   
+for _ in range(1):
+
+  print(data[data.index(b)])  #16
+  print(data[data.index(c)])  #17 
+  print(data[data.index(a)])  #18
+  print(data[data.index(b)])  #19 
+
+for _ in range(4):
+
+  print(data[data.index(a)])  #20     
+
+for _ in range(1):
+
+  print(data[data.index(c)])  #24
+  print(data[data.index(a)])  #25 
+  print(data[data.index(b)])  #26 
+  print(data[data.index(a)])  #27
+  print(data[data.index(b)])  #28  
+  print(data[data.index(a)])  #29
+  print(data[data.index(a)])  #30
+  print(data[data.index(c)])  #31
---
+++ office/근무표완성중(202403월)충전원.py
@@ -0,0 +1,62 @@
+
+a = {'주간': '이성용', '야간': '이재평', '휴무': '김기석'}
+b = {'주간': '김기석', '야간': '이성용', '휴무': '이재평'}
+c = {'주간': '이재평', '야간': '김기석', '휴무': '이성용'}
+d = {'주간': '     ', '야간': '이재평', '휴무': '이성용,김기석'}
+e = {'주간': '     ', '야간': '이성용', '휴무': '김기석,이재평'}
+f = {'주간': '     ', '야간': '김기석', '휴무': '이재평,이성용'}
+g = {'주간': '이성용', '야간': '김기석', '휴무': '이재평'}
+h = {'주간': '이재평', '야간': '이성용', '휴무': '김기석'}
+i = {'주간': '김기석', '야간': '이재평', '휴무': '이성용'}
+
+data = [a, b, c, d, e, f, g, h, i]
+
+# a ->x c, b ->x a, c ->x b,    
+# a ->x h, b ->x g, c ->x i   
+# d ->x b, e ->x a, f ->x c  
+# d ->x i, e ->x g, f ->x h
+# g ->x i, g ->x b
+# h->x g, h->x a
+# i ->x h, i ->x c
+
+for _ in range(1):
+
+  print(data[data.index(a)])   
+  
+for _ in range(2):
+  
+  print(data[data.index(b)])
+  print(data[data.index(b)]) 
+  print(data[data.index(c)])   
+  print(data[data.index(c)])
+  print(data[data.index(a)])
+  print(data[data.index(a)])
+  
+for _ in range(1):
+
+  print(data[data.index(b)])
+  print(data[data.index(b)]) 
+  print(data[data.index(c)])   
+  print(data[data.index(c)])
+  print(data[data.index(c)])
+  print(data[data.index(a)])  
+  print(data[data.index(a)])
+
+for _ in range(1):
+
+  print(data[data.index(b)])
+  print(data[data.index(b)]) 
+  print(data[data.index(c)])   
+  print(data[data.index(c)])
+  print(data[data.index(a)])
+  print(data[data.index(a)])  
+
+for _ in range(1):
+
+  print(data[data.index(b)])
+  print(data[data.index(b)]) 
+  print(data[data.index(c)])   
+  print(data[data.index(c)])
+  print(data[data.index(a)])
+  
+  
