beb7c56d0e279a832894ae2ea068311abcf253b9
editor-5 <kdcd2022@naver.com>
2024-03-21 13:13:18 +0900

    202403211312

--- tableau/tableau-excel-copysample.py
+++ tableau/tableau-excel-copysample.py
@@ -1,12 +1,19 @@
 
-a =  "='[일마감22.01월.xlsx]" 
+'''a =  "='[일마감22.01월.xlsx]" 
 b = '('
 c = 2
 d = ")'!$F$13"
 
 f = a + b + str(c) + d  
  
-for i in c:
-     c=+1
  
-print(f)
+print(f) '''
+
+a = "='[일마감23.12월.xlsx]"
+b = '('
+d = ")'!$F$13"
+
+# c값을 변경하면서 f를 출력
+for c in range(1, 32):
+    f = a + b + str(c) + d
+    print(f)
