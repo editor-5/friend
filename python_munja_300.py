84c4503b4c7163fdef5dfe24d37ed64c3a8ee4fa
editor-5 <kdcd2022@naver.com>
2024-04-26 18:28:14 +0900

    202404261828

---
+++ python_munja_300.py
@@ -0,0 +1,23 @@
+'''
+print("Hello World")
+print("Mary's cosmetics")
+print('신씨가 소리질렀다. "도둑이야".')
+print('C:\Windows')
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+print("오늘은", "일요일")
+print("naver", "kakao", "sk", "samsung", sep="/")
+print("first", end=""); print("second")
+print(5/3)
+
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+'''
